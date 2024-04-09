950d71842021c9c411f164548f85fcf79f5be7ca
Nathan Albuquerque <nathan.nra@outlook.com>
2024-04-09 18:16:23 -0300

    feat: added script for banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,15 @@ header_button_burguer.addEventListener('click', () => {
         header_nav.classList.remove('header__nav--show');
     }
 });
+
+/* ******* ******* ******* ******* ******* ******* ******* */
+/*                    3 - Banner Slider                    */
+/* ******* ******* ******* ******* ******* ******* ******* */
+
+$(document).ready(function () {
+    $('.banner__content').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
